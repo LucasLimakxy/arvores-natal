5c0984542730603622583cd6a7ebea93e351ffc7
Lucas <lukas_kxy@hotmail.com>
2020-03-11 19:40:14 -0300

    Avançando

--- index/models.py
+++ index/models.py
@@ -7,7 +7,7 @@ class Familia(models.Model):
     nome_popular = models.CharField(_('Nome Popular'), max_length=80)
     nome_cientifico = models.CharField(_('Nome Científico'), max_length=80)     
     descricao = models.TextField(null=True, verbose_name='Descrição')
-    imagem = models.ImageField(upload_to='media/familias/')
+    imagem = models.ImageField(upload_to='familias/')
     referencia = models.CharField(_('Referência'), max_length=80, null=True)
 
     def __str__(self):
@@ -19,7 +19,7 @@ class Arvore(models.Model):
     descricao = models.TextField(null=True, verbose_name='Descrição')
     referencia = models.CharField(_('Referência'), max_length=80, null=True) 
     familia =  models.ForeignKey(Familia, models.PROTECT, null=False)
-    imagem = models.ImageField(upload_to='media/arvores/')
+    imagem = models.ImageField(upload_to='arvores/')
 
     def __str__(self):
         return self.nome_popular
