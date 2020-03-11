130d5cddc7e7097535347543eab205f613be2272
Lucas <lukaskxy@gmail.com>
2020-03-11 13:02:04 -0300

    settinga

--- arvores_natal/urls.py
+++ arvores_natal/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from index import views
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),    
@@ -26,5 +27,6 @@ urlpatterns = [
     url('', views.DashboardView.as_view(), name='default'),
 ]
 
+urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
